fafa06a3f33c721400d454f37977218c5ab6f5b7
talsolovey <168472280+talsolovey@users.noreply.github.com>
2024-09-01 17:29:50 +0300

    added DOS attack prevention and updated products

--- express_server.js
+++ express_server.js
@@ -6,6 +6,14 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// rate limiter
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter); // apply to all requests
+
 // layout
 const expressLayouts = require('express-ejs-layouts');
 
