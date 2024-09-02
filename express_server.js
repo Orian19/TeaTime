40f07a7ff82da3bd40927da65e7c451d5b5d7402
talsolovey <168472280+talsolovey@users.noreply.github.com>
2024-09-02 11:23:31 +0300

    finished adding products, and extended rate limiter

--- express_server.js
+++ express_server.js
@@ -8,8 +8,8 @@ const port = 3000;
 
 // rate limiter
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+    windowMs: 30 * 60 * 1000, // 30 minutes
+    max: 1000 // limit each IP to 1000 requests per windowMs
 });
 
 app.use(limiter); // apply to all requests
